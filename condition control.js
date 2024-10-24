f1cbfad440db49e44907537a45d826aed26d6785
akashgitdev <nagaakashduddupudi1712@email.com>
2024-10-24 17:24:17 +0530

    commit

---
+++ condition control.js
@@ -0,0 +1,64 @@
+// var vstop="red";
+// var vstart="green";
+// var vready="yellow";
+// var light="yellow";
+// if(light== vstop)
+//     {
+//     console.log("stop")
+//     }
+//     else if(light==vstart){
+//         console.log("go")
+//     }
+//     else if(light==vready){
+//         console.log("ready to go")
+//     }
+//     else{
+//         console.log("invalid input")
+//     }
+
+// var i=33;
+// if(i==0){
+//     console.log("its frezzing cold") 
+// }
+// else if(i==15){
+//     console.log("its cool day")
+// }
+// else if(i==25){
+//     console.log("its pleasant day")
+// }
+// else{
+//     console.log("unknown weather")
+// }
+
+var i=6;
+if(i==1)
+{
+    console.log("today is monday")
+}
+else if(i==2)
+    {
+        console.log("today is tuesday")
+    }
+    else if(i==3)
+        {
+            console.log("today is wednesday")
+        }
+        else if(i==4)
+            {
+                console.log("today is thursday")
+            }
+            else if(i==5)
+                {
+                    console.log("today is friday")
+                }
+                else if(i==6)
+                    {
+                        console.log("today is saturday")
+                    }
+                    else if(i==7)
+                        {
+                            console.log("today is sunday")
+                        }
+                        else{
+                            console.log("invalid day number")
+                        }
